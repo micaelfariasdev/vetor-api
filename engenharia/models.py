27d3f56bc647a2467398ee41749effd71a3d1757
Micael Farias <Micaelfarias.dev@gmail.com>
2025-08-29 09:20:20 -0300

    tentando arrumar migração

--- engenharia/models.py
+++ engenharia/models.py
@@ -42,7 +42,7 @@ class Ponto(models.Model):
     saida_manha = models.TimeField(null=True, blank=True)
     saida_tarde = models.TimeField(null=True, blank=True)
     horas_trabalhadas = models.CharField(max_length=150, null=True, blank=True)
-    feriado = models.BooleanField(default=False)
+    feriado = models.BooleanField(default=False, verbose_name='Feriado?')
 
     class Meta:
         constraints = [
