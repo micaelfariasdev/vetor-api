f6d745ea818e6de0d7d214ba68ece372c00f6576
Micael Farias <Micaelfarias.dev@gmail.com>
2025-09-09 12:20:38 -0300

    configuração de autenticação

--- api/views/accounts.py
+++ api/views/accounts.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework import generics
 from django.contrib.auth.models import User
 from ..serializer import RegisterSerializer
-
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
