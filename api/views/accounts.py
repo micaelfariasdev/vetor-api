9d0aeaf716d5a7635486a0bc44fa37ac121ccb91
Micael Farias <Micaelfarias.dev@gmail.com>
2025-09-10 11:42:35 -0300

    MeView com nome e sobrenome

--- api/views/accounts.py
+++ api/views/accounts.py
@@ -79,6 +79,8 @@ class MeView(APIView):
             "id": user.id,
             "username": user.username,
             "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
         })
 
 
