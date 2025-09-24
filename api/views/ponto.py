4dfbf562916a4f0b035f8a45026cfd631466c018
Micael Farias <Micaelfarias.dev@gmail.com>
2025-09-24 15:44:08 -0300

    correção em salvar pontos

--- api/views/ponto.py
+++ api/views/ponto.py
@@ -414,7 +414,7 @@ def pdf_pontos_relatorio(request, mes_id, col=None):
                 }
                 resultado.append(data)
 
-        
+        return HttpResponse(resultado, status=200)
         if len(resultado) > 1:
             pdf = requests.post(
             'http://64.181.171.161/gerar-pdf', json=resultado)
